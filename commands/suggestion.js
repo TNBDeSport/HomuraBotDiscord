3fdc61c97a3106db539e1820d734c265ce354ad8
TNBDeSport <51657758+TNBDeSport@users.noreply.github.com>
2019-08-08 13:56:54 +0200

    Update suggestion.js

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -1,18 +1,23 @@
  const Discord = require('discord.js');
 exports.run = (client, message, args) => {
   
-
-
+let msg = args.slice(1).join(' ');
+if (msg.length < 1) return message.reply('Utilisation :`h!suggestion <message>`.');
+ 
   let suggestchan = client.channels.find('id', '557936381834297345');
       const embed = new Discord.RichEmbed()
 
       .setColor("#3C2A66")
       .setTitle(`${message.author.username}`)
-      .setDescription(args.join(" "))
+      .setDescription(msg)
       
 
 
-      suggestchan.send(embed)
+      suggestchan.send(embed).then( rm => {
+        rm.react(client.emojis.find('id', '543783599883681825'));
+        rm.react(client.emojis.find('id', '543784274650726403'));
+      }
+                                   
       suggestchan.send("<@302837596600664065>").then(Message => {
 Message.delete()
 });
